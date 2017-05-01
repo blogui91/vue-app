3325ffd8f79a52133e31a667043506e9b83be1cf
Cesar Santana <casc.santana@gmail.com>
2017-04-30 23:26:39 -0500

    reestructing files

---
+++ src/resources/lang/strings.js
@@ -0,0 +1,39 @@
+export default {
+	es: {
+		error_responses: {
+			401: "Usuario o contraseña inválidos",
+			500: "Error en el servidor, intente nuevamente",
+			422 : "No pudimos realizar esta operación",
+			404 : "No pudimos encontrar el recurso",
+		},
+		buttons: {
+			accept: "Aceptar",
+			ok: "Ok",
+			cancel: "Cancelar",
+			delete: "Eliminar",
+			continue: "Continuar",
+			close: "Cerrar",
+			done: "Hecho",
+			start: "Iniciar",
+		}
+	},
+	en: {
+		error_responses: {
+			401: "User or password invalid",
+			500: "Server error, try again!",
+			422 : "We could not do this operation",
+			404 : "We could not find the resource"
+		},
+		buttons: {
+			accept: "Accept",
+			ok: "Ok",
+			cancel: "Cancel",
+			delete: "Delete",
+			continue: "Continue",
+			close: "Close",
+			done: "Done",
+			start: "Start",
+
+		}
+	}
+}
